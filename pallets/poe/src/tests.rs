f32a5a9b11738e8b56f8a7ff8f9e136de4ee2fc1
Xiankun Cheng <xiankuncheng@Xiankuns-MacBook-Pro-2.local>
2021-12-20 14:08:10 +1300

    feat: tests for happy + sad paths for create/revoke claim.

--- pallets/poe/src/tests.rs
+++ pallets/poe/src/tests.rs
@@ -1,20 +1,52 @@
-use crate::{mock::*, Error};
+use crate::{mock::*, Error, Proofs};
 use frame_support::{assert_noop, assert_ok};
 
 #[test]
-fn it_works_for_default_value() {
+fn create_claim_works() {
 	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(TemplateModule::something(), Some(42));
+		let claim = vec![0, 1];
+		assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+		assert_eq!(
+			Proofs::<Test>::get(&claim),
+			Some((1, <frame_system::Pallet<Test>>::block_number()))
+		)
 	});
 }
 
 #[test]
-fn correct_error_for_none_value() {
+fn create_claim_failed_when_claim_already_exist() {
 	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
-		assert_noop!(TemplateModule::cause_error(Origin::signed(1)), Error::<Test>::NoneValue);
-	});
+		let claim = vec![0, 1];
+		let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+		assert_noop!(
+			PoeModule::create_claim(Origin::signed(1), claim.clone()),
+			Error::<Test>::ProofAlreadyExist
+		);
+	})
+}
+
+#[test]
+fn revoke_claim_works() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+		let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+		assert_ok!(PoeModule::revoke_claim(Origin::signed(1), claim.clone()));
+		assert_eq!(Proofs::<Test>::get(&claim), None);
+	})
+}
+
+#[test]
+fn revoke_claim_failed_when_claim_is_not_exist() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+
+		assert_noop!(
+			PoeModule::revoke_claim(Origin::signed(1), claim.clone()),
+			Error::<Test>::ClaimNotExist
+		);
+	})
+}
+
 }
