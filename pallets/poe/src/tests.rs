41b434a47a1df3dc00e7dcbed30c6caea0b28bf9
Xiankun Cheng <xiankuncheng@Xiankuns-MacBook-Pro-2.local>
2021-12-20 14:08:41 +1300

    chore: add test for origin is not claim owner.

--- pallets/poe/src/tests.rs
+++ pallets/poe/src/tests.rs
@@ -49,4 +49,16 @@ fn revoke_claim_failed_when_claim_is_not_exist() {
 	})
 }
 
+#[test]
+fn revoke_claim_failed_when_origin_is_not_claim_owner() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+
+		let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+		assert_noop!(
+			PoeModule::revoke_claim(Origin::signed(2), claim.clone()),
+			Error::<Test>::NotClaimOwner
+		);
+	})
 }
