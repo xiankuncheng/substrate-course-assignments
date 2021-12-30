3951d578413ae2bde738ce2c2bd0f0930ebaca48
Xiankun Cheng <xiankuncheng@Xiankuns-MacBook-Pro-2.local>
2021-12-30 22:03:31 +1300

    feat+refactor: add buy and sell extrinsics and refactor existing codes.

--- lesson02/pallets/kitties/src/lib.rs
+++ lesson02/pallets/kitties/src/lib.rs
@@ -38,7 +38,7 @@ pub mod pallet {
 
 	#[pallet::storage]
 	#[pallet::getter(fn kitties_count)]
-	pub type KittiesCount<T> = StorageValue<_, u32>;
+	pub type KittiesCount<T> = StorageValue<_, u32, ValueQuery>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn kitties)]
@@ -71,19 +71,11 @@ pub mod pallet {
 		pub fn create(origin: OriginFor<T>) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 
-			let kitty_id = match Self::kitties_count() {
-				Some(id) => {
-					ensure!(id != KittyIndex::MAX, Error::<T>::KittiesCountOverflow);
-					id
-				}
-				None => 1,
-			};
-
+			let kitty_id =
+				Self::kitties_count().checked_add(1).ok_or(Error::<T>::KittiesCountOverflow)?;
 			let dna = Self::random_value(&who);
 
-			Kitties::<T>::insert(kitty_id, Some(Kitty(dna)));
-			Owner::<T>::insert(kitty_id, Some(who.clone()));
-			KittiesCount::<T>::put(kitty_id + 1);
+			Self::create_kitty(kitty_id, dna, &who);
 
 			Self::deposit_event(Event::KittyCreate(who, kitty_id));
 
@@ -127,13 +119,8 @@ pub mod pallet {
 			Owner::<T>::get(kitty_id2).ok_or(Error::<T>::NotOwner)?;
 
 			// get kitty id
-			let kitty_id = match Self::kitties_count() {
-				Some(id) => {
-					ensure!(id != KittyIndex::MAX, Error::<T>::KittiesCountOverflow);
-					id
-				}
-				None => 1,
-			};
+			let kitty_id =
+				Self::kitties_count().checked_add(1).ok_or(Error::<T>::KittiesCountOverflow)?;
 
 			// dna generate
 			let dna_1 = kitty1.0;
@@ -146,11 +133,45 @@ pub mod pallet {
 				new_dna[i] = (selector[i] & dna_1[i]) | (!selector[i] & dna_2[i]);
 			}
 
-			Kitties::<T>::insert(kitty_id, Some(Kitty(new_dna)));
-			Owner::<T>::insert(kitty_id, Some(who.clone()));
-			KittiesCount::<T>::put(kitty_id + 1);
+			Self::create_kitty(kitty_id, new_dna, &who);
 
 			Self::deposit_event(Event::KittyCreate(who, kitty_id));
+
+			Ok(())
+		}
+
+		#[pallet::weight(0)]
+		pub fn buy(
+			origin: OriginFor<T>,
+			kitty_id: KittyIndex,
+			from: T::AccountId,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+
+			let kitty_host = Owner::<T>::get(kitty_id).ok_or(Error::<T>::InvalidKittyIndex)?;
+			ensure!(kitty_host == from, Error::<T>::NotOwner);
+
+			Owner::<T>::insert(kitty_id, Some(who.clone()));
+
+			Self::deposit_event(Event::KittyTransfer(from, who, kitty_id));
+
+			Ok(())
+		}
+
+		#[pallet::weight(0)]
+		pub fn sell(
+			origin: OriginFor<T>,
+			kitty_id: KittyIndex,
+			dest: T::AccountId,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+
+			let kitty_host = Owner::<T>::get(kitty_id).ok_or(Error::<T>::InvalidKittyIndex)?;
+			ensure!(kitty_host == who, Error::<T>::NotOwner);
+
+			Owner::<T>::insert(kitty_id, Some(who.clone()));
+
+			Self::deposit_event(Event::KittyTransfer(who, dest, kitty_id));
 
 			Ok(())
 		}
@@ -164,6 +185,12 @@ pub mod pallet {
 				<frame_system::Pallet<T>>::extrinsic_index(),
 			);
 			payload.using_encoded(blake2_128)
+		}
+
+		fn create_kitty(kitty_id: u32, dna: [u8; 16], who: &T::AccountId) {
+			Kitties::<T>::insert(kitty_id, Some(Kitty(dna)));
+			Owner::<T>::insert(kitty_id, Some(who.clone()));
+			KittiesCount::<T>::put(kitty_id);
 		}
 	}
 }
